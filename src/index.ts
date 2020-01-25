524180777f4191333f256fca6e2a4b36a0ed48a9
Ninad <m.ninad99@gmail.com>
2020-01-26 00:15:00 +0200

    added unit customization

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,33 @@ import Joi from '@hapi/joi';
 import { Extension, RuleArgs } from '@hapi/joi';
 import moment from 'moment';
 
+const VALID_MOMENT_UNIT = [
+    'year',
+    'years',
+    'y',
+    'quarter',
+    'quarters',
+    'Q',
+    'month',
+    'months',
+    'M',
+    'week',
+    'weeks',
+    'w',
+    'day',
+    'days',
+    'd',
+    'hour',
+    'hours',
+    'h',
+    'minute',
+    'minutes',
+    'm',
+    'millisecond',
+    'milliseconds',
+    'ms',
+];
+
 export default function relativeDateValidator(relativeTo: Date | string = 'today'): Extension {
     if (relativeTo !== 'today') {
         if (!moment(relativeTo).isValid()) {
@@ -12,29 +39,35 @@ export default function relativeDateValidator(relativeTo: Date | string = 'today
         type: 'relativeDate',
         base: Joi.date(),
         messages: {
-            'relateiveDate.before': 'date must be before {{#days}} from {{#from}}',
-            'relateiveDate.after': 'date must be before {{#days}} from {{#from}}',
+            'relateiveDate.before': 'date must be before {{#value}} {{#unit}} from {{#from}}',
+            'relateiveDate.after': 'date must be after {{#value}} {{#unit}} from {{#from}}',
         },
         rules: {
             before: {
                 alias: 'lessThan',
-                method(days) {
-                    return this.$_addRule({ name: 'before', args: { days } });
+                method(value, unit) {
+                    return this.$_addRule({ name: 'before', args: { value, unit } });
                 },
                 args: [
                     {
-                        name: 'days',
+                        name: 'value',
                         ref: true,
                         assert: (value) => typeof value === 'number' && !isNaN(value),
                         message: 'must be a number',
                     },
+                    {
+                        name: 'unit',
+                        assert: (unit) => isValidUnit(unit),
+                        message: 'must be a valid date unit',
+                    },
                 ],
                 validate(value, helpers, args, options) {
-                    const mustBeBefore = getRelativeToDate().add(args.days, 'days');
+                    const mustBeBefore = getRelativeToDate().add(args.value, args.unit);
                     const toValidate = moment(value);
                     if (!toValidate.isBefore(mustBeBefore)) {
                         return helpers.error('relateiveDate.before', {
-                            days: args.days,
+                            value: args.value,
+                            unit: moment.normalizeUnits(args.unit),
                             from: getRelativeToDate().format('DD-MM-YYYY'),
                         });
                     }
@@ -42,15 +75,20 @@ export default function relativeDateValidator(relativeTo: Date | string = 'today
             },
             after: {
                 alias: 'greaterThan',
-                method(days) {
-                    return this.$_addRule({ name: 'after', args: { days } });
+                method(value, unit) {
+                    return this.$_addRule({ name: 'after', args: { value, unit } });
                 },
                 args: [
                     {
-                        name: 'days',
+                        name: 'value',
                         ref: true,
                         assert: (value) => typeof value === 'number' && !isNaN(value),
                         message: 'must be a number',
+                    },
+                    {
+                        name: 'unit',
+                        assert: (unit) => isValidUnit(unit),
+                        message: 'must be a valid date unit',
                     },
                 ],
                 validate(value, helpers, args, options) {
@@ -66,6 +104,7 @@ export default function relativeDateValidator(relativeTo: Date | string = 'today
             },
         },
     };
+
     function getRelativeToDate() {
         if (relativeTo === 'today') {
             return moment();
@@ -73,11 +112,17 @@ export default function relativeDateValidator(relativeTo: Date | string = 'today
             return moment(relativeTo);
         }
     }
+
+    function isValidUnit(unit: string): boolean {
+        return VALID_MOMENT_UNIT.includes(unit);
+    }
 }
 
-const custom = Joi.extend(relativeDateValidator(moment('01-07-2019', 'DD-MM-YYYY').toDate()));
+const custom = Joi.extend(relativeDateValidator());
 
 const schema = Joi.object({
-    dueDate: custom.relativeDate().before('a'),
+    dueDate: custom.relativeDate().before(2, 'y'),
 });
-const x = schema.validate({ dueDate: '2019-01-01' });
+const x = schema.validate({ dueDate: '2018-01-01' });
+
+// console.log(x);
